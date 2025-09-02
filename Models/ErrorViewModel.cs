2d94f5418470b215910814951db53962b1eff231
Ram660-afk <ramkmr2314@gmail.com>
2025-09-02 09:29:29 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace project.asc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
